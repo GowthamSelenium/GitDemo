035a52207f333c47ed91b20578a839ee6b4a9ed1
Gowtham <gowthammurugesan20081998@outlook.com>
2024-03-06 17:13:53 +0530

    This is a commit from gowtham1

--- src/Basic_Concepts/letter_count.java
+++ src/Basic_Concepts/letter_count.java
@@ -5,7 +5,7 @@ public class letter_count {
 	public static void main(String[] args) {
 		String str = "Hello";
 		char[] array = str.toCharArray();
-		char lettercount = 'l';
+		char lettercount = '2';
 		int count = 0;
 		for(int i=0; i<array.length;i++) {
 			if(array[i]==lettercount) {
